e4d50e4c34ab62478dc0f63726058b0b962961c4
Lucas-S-Lima <2ucas2ima@gmail.com>
2026-03-01 14:57:18 -0300

    Increase test

--- app/tests/test_items.py
+++ app/tests/test_items.py
@@ -2,6 +2,7 @@ import pytest
 from django.urls import reverse
 from app.models import Item
 
+
 @pytest.mark.django_db
 def test_create_item(client):
     response = client.post(reverse('item_create'), data={
@@ -11,12 +12,14 @@ def test_create_item(client):
     assert response.status_code == 201
     assert Item.objects.filter(name='Test Item').exists()
 
+
 @pytest.mark.django_db
 def test_list_items(client):
     Item.objects.create(name='Item1', description='Desc1')
     response = client.get(reverse('item_list'))
     assert response.status_code == 200
     assert len(response.json()) >= 1
+
 
 @pytest.mark.django_db
 def test_update_item(client):
@@ -30,9 +33,11 @@ def test_update_item(client):
     item.refresh_from_db()
     assert item.name == 'New'
 
+
 @pytest.mark.django_db
 def test_delete_item(client):
     item = Item.objects.create(name='ToDelete', description='')
     response = client.delete(reverse('item_delete', args=[item.id]))
     assert response.status_code == 200
-    assert not Item.objects.filter(id=item.id).exists()
+    item_exists = Item.objects.filter(id=item.id).exists()
+    assert not item_exists
